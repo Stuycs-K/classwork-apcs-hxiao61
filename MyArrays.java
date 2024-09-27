9c09405c914bdb98794c3a28a1b89fd39e478df1
hxiao61 <hxiao61@stuy.edu>
2024-09-27 10:10:47 -0400

    arytostring and main

---
+++ MyArrays.java
@@ -0,0 +1,16 @@
+public class MyArrays{
+  public static String aryToString(int[] nums){
+    String out = "[";
+    for(int i = 0; i < nums.length; i++){
+      out += nums[i];
+      if (i != nums.length - 1){
+        out += ", ";
+      }
+      out += "]"
+      return out;
+    }
+  }
+  public static int[] returnCopy(int[]ary){
+    
+  }
+}
