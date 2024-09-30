661c9a77e069f44cae4cf5682793749a2bf23f92
hxiao61 <hxia61@stuy.edu>
2024-09-29 23:10:36 -0400

    did concat and formatted it

--- MyArrays.java
+++ MyArrays.java
@@ -1,22 +1,29 @@
-public class MyArrays{
-  public static void main(String[]args){
-    
-  }
-  public static String aryToString(int[] nums) {
-    String out = "[";
-    for (int i = 0; i < nums.length; i++) {
-        out += nums[i];
-        if (i != nums.length - 1) {
-            out += ", ";
-        }
+public class MyArrays {
+    public static void main(String[] args) {
+
     }
-    out += "]";
-    return out;
-}
-  public static int[] returnCopy(int[]ary){
+    public static String aryToString(int[] nums) {
+        String out = "[";
+        for (int i = 0; i < nums.length; i++) {
+            out += nums[i];
+            if (i != nums.length - 1) {
+                out += ", ";
+            }
+        }
+        out += "]";
+        return out;
+    }
+    public static int[] returnCopy(int[] ary) {
 
-  }
-  public static int[] concatArray(int[]ary1,int[]ary2){
-
-  }
+    }
+    public static int[] concatArray(int[] ary1, int[] ary2) {
+        int[] news = new int[ary1.length + ary2.length];
+        for (int i = 0; i < ary1.length; i++) {
+            news[i] = ary1[i];
+        }
+        for (int i = 0; i < ary2.length; i++) {
+            news[ary1.length + i] = ary2[i];
+        }
+        return news;
+    }
 }
