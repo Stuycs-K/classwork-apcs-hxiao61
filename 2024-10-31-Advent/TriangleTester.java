0631863963c311d12d105ae266f9c3f74eaf9109
hxiao61 <hxiao61@stuy.edu>
2024-11-04 10:04:34 -0500

    finished istriangle

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -2,5 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class TriangleTester{
-  public static int countTrianglesA(String filename)
+  public static boolean isTriangle(int a, int b, int c){
+    return ((a + b) > c && (a + c) > b && (c + b) > a);
+  }
+  public static int countTrianglesA(String filename);
 }
