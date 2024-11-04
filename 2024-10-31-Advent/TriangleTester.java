a83687ab3e106bfe26705777fb6e40340237fb77
hxiao61 <hxiao61@stuy.edu>
2024-11-04 10:13:10 -0500

    almost done with counttriangle

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -5,5 +5,22 @@ public class TriangleTester{
   public static boolean isTriangle(int a, int b, int c){
     return ((a + b) > c && (a + c) > b && (c + b) > a);
   }
-  public static int countTrianglesA(String filename);
+  public static int countTrianglesA(String filename){
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      while(input.hasNextInt()){
+        int result = 0;
+        if(isTriangle(input.nextInt(), input.nextInt(), input.nextInt())){
+          result++;
+        }
+      }
+      return result;
+  }
+  catch (FileNotFoundException ex) {
+    //File not found what should you do?
+    System.out.println("File not found");
+    return; //you can return from a void function just don't put a value.
+  }
+}
 }
