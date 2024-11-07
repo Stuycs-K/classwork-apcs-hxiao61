7f8d490a74cfd9bdde2bf3900d04b808e575a567
hxiao61 <hxiao61@stuy.edu>
2024-11-07 10:05:02 -0500

    finished constructor and bird

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -2,5 +2,7 @@ public class Driver{
   public static void main(String[] args){
     Animal cat = new Animal("meow", 2, "sigma");
     cat.speak();
+    Bird hawk = new Bird("hawk 2", 2, "uh", 6.0, "red");
+    hawk.speak();
   }
 }
