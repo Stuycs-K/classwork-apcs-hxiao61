857b86ce7556d1b4ef61601c99ab959b0b7a8313
hxiao61 <hxiao61@stuy.edu>
2024-11-14 08:59:34 -0600

    did 2 funcs

--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -1,24 +1,36 @@
+import java.util.ArrayList;
 public class Driver{
-  public static main(String[] args){
+  public static void main(String[] args){
+    System.out.println(createRandomArray(10));
 
   }
   public static ArrayList<String>createRandomArray(int size){
-    ArrayList<String> Arr = new ArrayList[size];
-    for (int i = 0; i < size; i++){
+    ArrayList<String> Arr = new ArrayList<String>(size);
+    for (int i = 0; i < size -1; i++){
       int temp = (int)(Math.random() * 11);
-      Arr[i] = "" + temp;
+      Arr.add("" + temp);
+
     }
+    replaceEmpty(Arr);
+    return Arr;
   }
   public static void replaceEmpty( ArrayList<String> original){
   //Modify the ArrayList such that it has all of the empty strings are
   //replaced with the word "Empty".
+  for(int i =0; i < original.size(); i++){
+    if(original.get(i).equals("0")){
+      original.set(i, "Empty");
+    }
+  }
   }
   public static ArrayList<String> makeReversedList( ArrayList<String> original){
     //return a new ArrayList that is in the reversed order of the original.
+    return null;
   }
   public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+    return null;
     //return a new ArrayList that has all values of a and b in alternating order that is:
     //a[0], b[0], a[1], b[1]...
     //If one list is longer than the other, just attach the remaining values to the end.
-  }   
+  }
 }
