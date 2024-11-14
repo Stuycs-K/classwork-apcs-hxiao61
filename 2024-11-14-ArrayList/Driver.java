09312fa6f79115162ae7d0eb82ed5f834baa09ab
hxiao61 <hxiao61@stuy.edu>
2024-11-14 09:11:33 -0600

    finished the lab

--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -1,13 +1,16 @@
 import java.util.ArrayList;
 public class Driver{
   public static void main(String[] args){
-    ArrayList<String> tester = createRandomArray(20);
+    ArrayList<String> tester = createRandomArray(10);
+    ArrayList<String> tester2 = createRandomArray(5);
     System.out.println(tester);
-    System.out.println(makeReversedList(tester));
+    //System.out.println(makeReversedList(tester));
+    System.out.println(tester2);
+    System.out.println(mixLists(tester, tester2));
   }
   public static ArrayList<String>createRandomArray(int size){
     ArrayList<String> Arr = new ArrayList<String>(size);
-    for (int i = 0; i < size -1; i++){
+    for (int i = 0; i < size; i++){
       int temp = (int)(Math.random() * 11);
       Arr.add("" + temp);
 
@@ -32,7 +35,23 @@ public class Driver{
     return Arr;
   }
   public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
-    return null;
+    ArrayList<String> Arr = new ArrayList<String>(a.size() + b.size());
+    for (int i = 0; i < Math.min(a.size(), b.size()); i++){
+      Arr.add(a.get(i));
+      Arr.add(b.get(i));
+    }
+    if (a.size() > b.size()){
+      for(int i = b.size(); i < a.size(); i++){
+        Arr.add(a.get(i));
+      }
+      }
+      if (b.size() > a.size()){
+        for(int i = a.size(); i < b.size(); i++){
+          Arr.add(b.get(i));
+        }
+    }
+return Arr;
+
     //return a new ArrayList that has all values of a and b in alternating order that is:
     //a[0], b[0], a[1], b[1]...
     //If one list is longer than the other, just attach the remaining values to the end.
