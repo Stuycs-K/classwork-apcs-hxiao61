b2e7682583df50c9f6d2e38189a0e52cdfa8e0d7
hxiao61 <hxiao61@stuy.edu>
2024-11-19 08:49:35 -0600

    going to fix some errors now

--- 2024-11-18-advent2016/Driver.java
+++ 2024-11-18-advent2016/Driver.java
@@ -1,31 +1,59 @@
 public class Driver{
+  public static String[]parse(String file){
+    try{
+      Scanner inf = new Scanner(new File "input.txt");
+      Scanner line = inf.nextLine();
+      System.out.println(line);
+      return null;
+    } catch(Exception e){
+      System.exit(1);
+    }
+    return null;
+  }
   public void main(String[] arg){
+    String[]data = parse("input.txt");
 
   }
-  public static int day1(Array arr){
+  public static int day1(String[]data){
     int count = 0;
     int forward = 0;
     int backward = 0;
     int right = 0;
     int left = 0;
     int direction = 1;
-    for (int i =0; i < arr.legnth(); i++){
-      int current = parseInt(arr[i].substring(1));
-      if(arr[i].has("R")){
+    for (int i =0; i < data.legnth(); i++){
+      int current = parseInt(data[i].substring(1));
+      if(data[i].has("R")){
         direction++;
+        if(direction == 1){
+          forward = forward+current;
+        if(direction == 2){
+          right = right+current
+        }
+        if(direction == 3){
+          backward = backward+current;
+        }
+        if(direction == 4){
+          left = left+current;
+        }
       }
-      if(direction == 1){
-        forward++;
-      if(direction == 2){
-        right++;
       }
-      if(direction == 3){
-        backward++;
-      }
-      if(direction == 4){
-        left++;
+      if(data[i].has("L")){
+        direction--;
+        if(direction == 1){
+          forward = forward+current;
+        if(direction == 2){
+          right = right+current
+        }
+        if(direction == 3){
+          backward = backward+current;
+        }
+        if(direction == 4){
+          left = left+current;
+        }
       }
       }
     }
+    return(Math.abs(right-left) + Math.abs(forward-backward));
   }
 }
