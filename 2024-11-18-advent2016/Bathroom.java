a7b2c26023e989f9c1e2b92f506d20a6b3ac9416
hxiao61 <hxia61@stuy.edu>
2024-11-19 20:35:10 -0500

    starting it and did part of it and have an idea of what im goign to do

--- 2024-11-18-advent2016/Bathroom.java
+++ 2024-11-18-advent2016/Bathroom.java
@@ -8,7 +8,24 @@ public class Driver{
 
     }
     public static String bathroom(String[]data){
-        
+        String[][] keypad = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int row = 1;
+        int col = 1;
+        String code = "";
+        for(int i = 0; i < data.length; i++){
+            if(data[i] == "U"){
+                row--;
+            }
+            if(data[i] == "D"){
+                row++;
+            }
+            if(data[i] == "L"){
+                col--;
+            }
+            if(data[i] == "R"){
+                col++;
+            }
+        }
     }
 
 }
