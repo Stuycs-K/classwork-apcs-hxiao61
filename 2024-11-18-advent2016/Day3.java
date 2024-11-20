5166ae8b9ba4af19c652cf1732047c4727647ab1
hxiao61 <hxiao61@stuy.edu>
2024-11-20 09:12:14 -0600

    getting started and have an idea on what ot do

---
+++ 2024-11-18-advent2016/Day3.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Day3{
+  public static void main(String[] arg){
+
+  }
+  public static int day3(String code){
+    int sector = 0;
+    String first = "";
+    String second = "";
+    String third = "";
+    String fourth = "";
+    String fifth = "";
+    for(int i = 0; i < code.length(); i++){
+      
+    }
+  }
+}
