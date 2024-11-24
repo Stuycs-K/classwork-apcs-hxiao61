20176c4c8c0b62a32a89d20a3430e9865e26922f
hxiao61 <hxia61@stuy.edu>
2024-11-24 14:38:20 -0500

    finally finished

--- 2024-11-18-advent2016/day6.java
+++ 2024-11-18-advent2016/day6.java
@@ -1,42 +1,63 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
 public class day6{
     public static void main(String[] args){
-
+        String[] data = parse("day6.txt");
+        System.out.println(day6(data));
+        }
+    public static String[] parse(String file) {
+        try {
+            Scanner inf = new Scanner(new File(file));
+            ArrayList<String> lines = new ArrayList<>();
+            while (inf.hasNextLine()) {
+                String line = inf.nextLine();
+                lines.add(line.trim());
+            }
+            inf.close();
+            return lines.toArray(new String[0]);
+        } catch (Exception e) {
+            System.exit(1);
+        }
+        return null;
     }
     public static String day6(String[] data){
         String result = "";
         for(int i = 0; i < data[0].length(); i++){
-            int a = 0, b = 0, c =0, d = 0, e = 0, f = 0, g = 0, h = 0, i = 0, j = 0, k = 0, l = 0, m = 0, n = 0, o = 0, p = 0, q = 0, r = 0, s = 0, t = 0, u = 0, v = 0, w = 0, x = 0, y = 0, z = 0;
-            for(int row = 0; row < messages.length; row++){
-                char c = messages[row].charAt(i);
-                if (c == 'a') a++;
-                else if (c == 'b') b++;
-                else if (c == 'c') c++;
-                else if (c == 'd') d++;
-                else if (c == 'e') e++;
-                else if (c == 'f') f++;
-                else if (c == 'g') g++;
-                else if (c == 'h') h++;
-                else if (c == 'i') i++;
-                else if (c == 'j') j++;
-                else if (c == 'k') k++;
-                else if (c == 'l') l++;
-                else if (c == 'm') m++;
-                else if (c == 'n') n++;
-                else if (c == 'o') o++;
-                else if (c == 'p') p++;
-                else if (c == 'q') q++;
-                else if (c == 'r') r++;
-                else if (c == 's') s++;
-                else if (c == 't') t++;
-                else if (c == 'u') u++;
-                else if (c == 'v') v++;
-                else if (c == 'w') w++;
-                else if (c == 'x') x++;
-                else if (c == 'y') y++;
-                else if (c == 'z') z++;
+            int a = 0, b = 0, c =0, d = 0, e = 0, f = 0, g = 0, h = 0, ic = 0, j = 0, k = 0, l = 0, m = 0, n = 0, o = 0, p = 0, q = 0, r = 0, s = 0, t = 0, u = 0, v = 0, w = 0, x = 0, y = 0, z = 0;
+            for(int row = 0; row < data.length; row++){
+                char ch = data[row].charAt(i);
+                if (ch == 'a') a++;
+                else if (ch == 'b') b++;
+                else if (ch == 'c') c++;
+                else if (ch == 'd') d++;
+                else if (ch == 'e') e++;
+                else if (ch == 'f') f++;
+                else if (ch == 'g') g++;
+                else if (ch == 'h') h++;
+                else if (ch == 'i') ic++;
+                else if (ch == 'j') j++;
+                else if (ch == 'k') k++;
+                else if (ch == 'l') l++;
+                else if (ch == 'm') m++;
+                else if (ch == 'n') n++;
+                else if (ch == 'o') o++;
+                else if (ch == 'p') p++;
+                else if (ch == 'q') q++;
+                else if (ch == 'r') r++;
+                else if (ch == 's') s++;
+                else if (ch == 't') t++;
+                else if (ch == 'u') u++;
+                else if (ch == 'v') v++;
+                else if (ch == 'w') w++;
+                else if (ch == 'x') x++;
+                else if (ch == 'y') y++;
+                else if (ch == 'z') z++;
             }
             int max = 0;
-            char fre = '';
+            char fre = ' ';
             if (a > max) { max = a; fre = 'a'; }
             if (b > max) { max = b; fre = 'b'; }
             if (c > max) { max = c; fre = 'c'; }
@@ -45,7 +66,7 @@ public class day6{
             if (f > max) { max = f; fre = 'f'; }
             if (g > max) { max = g; fre = 'g'; }
             if (h > max) { max = h; fre = 'h'; }
-            if (i > max) { max = i; fre = 'i'; }
+            if (ic > max) { max = ic; fre = 'i'; }
             if (j > max) { max = j; fre = 'j'; }
             if (k > max) { max = k; fre = 'k'; }
             if (l > max) { max = l; fre = 'l'; }
