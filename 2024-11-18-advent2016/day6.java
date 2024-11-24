cbe70d19bf9152431d89719fbd20c6cd9cd3f345
hxiao61 <hxia61@stuy.edu>
2024-11-24 14:16:25 -0500

    i think ig ot a stupid method

---
+++ 2024-11-18-advent2016/day6.java
@@ -0,0 +1,11 @@
+public class day6{
+    public static void main(String[] args){
+
+    }
+    public static String day6(String[] data){
+        String result = "";
+        for(int i = 0; i < data[0].length(); i++){
+            int a = 0, b = 0;
+        }
+    }
+}
