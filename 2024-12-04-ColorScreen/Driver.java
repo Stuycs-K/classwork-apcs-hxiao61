f45e4baa58b371490898a20b42168836beb60006
hxiao61 <hxia61@stuy.edu>
2024-12-05 02:49:35 -0500

    i need to finish the rest at school

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -4,7 +4,7 @@ public class Driver{
     public static int row = 31;
     public static int col = 80;
     public static void drawborder(){
-        Text.color(Text.BLUE);
+        Text.color(Text.BLUE, Text.background(Text.MAGENTA), Text.BRIGHT);
         for (int i = 1; i <= col; i++){
             Text.go(1, i);
             System.out.print("O");
@@ -22,10 +22,32 @@ public class Driver{
     public static random(){
         return (int)(Math.random() * (99 - 0 + 1)) + 0;
     }
+    public static void display(){
+        for(int i = 0; i < 3; i++){
+            int num = random();
+            if(num < 25){
+                Text.color(Text.RED, Text.BRIGHT);
+            } else if (num > 75) {
+                Text.color(Text.GREEN, Text.BRIGHT);
+            } else {
+                Text.color(Text.WHITE);
+            }
+            System.out.print(number);
+        }
+    }
+    public static void seperate(){
+        Text.color(Text.BLUE, Text.background(Text.MAGENTA), Text.BRIGHT);
+        for(int i = 1; i <= col; i++){
+            System.out.print(".");
+        }
+    }
     public static void main(String[] args) {
         System.out.println(Text.CLEAR_SCREEN);
         System.out.println(Text.HIDE_CURSOR);
         drawborder();
-        random()
+        display();
+        seperate();
+        Text.go(31);
+        System.out.println(Text.RESET);
     }
 }
