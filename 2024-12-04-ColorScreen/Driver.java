549b0020854f24cb1986c5495987b67866e86a8d
hxiao61 <hxia61@stuy.edu>
2024-12-05 02:27:11 -0500

    idk is this how you do it

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,7 +1,27 @@
 import java.util.Random;
 
 public class Driver{
+    public static int row = 31;
+    public static int col = 80;
+    public static void drawborder(){
+        Text.color(Text.BLUE);
+        for (int i = 1; i < col; i++){
+            Text.go(1, i);
+            System.out.print("O");
+            Text.go(row, i);
+            System.out.print("O");
+        }
+        for(int i = 1; i < row; i++){
+            Text.go(i, 1);
+            System.out.print("O");
+            Text.go(i, col);
+            System.out.print("O");
+        }
+    }
     public static void main(String[] args) {
-
+        System.out.println(Text.CLEAR_SCREEN);
+        System.out.println(Text.HIDE_CURSOR);
+        drawborder();
+        System.out.print(Text.RESET);
     }
 }
