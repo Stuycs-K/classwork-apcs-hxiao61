e0836892f10cd484f80768ce2067ca6a23e17049
hxiao61 <hxia61@stuy.edu>
2024-12-05 02:35:56 -0500

    fixed errors and made random

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -5,23 +5,27 @@ public class Driver{
     public static int col = 80;
     public static void drawborder(){
         Text.color(Text.BLUE);
-        for (int i = 1; i < col; i++){
+        for (int i = 1; i <= col; i++){
             Text.go(1, i);
             System.out.print("O");
             Text.go(row, i);
             System.out.print("O");
         }
-        for(int i = 1; i < row; i++){
+        for(int i = 1; i <= row; i++){
             Text.go(i, 1);
             System.out.print("O");
             Text.go(i, col);
             System.out.print("O");
         }
+        System.out.print(Text.RESET);
+    }
+    public static random(){
+        return (int)(Math.random() * (99 - 0 + 1)) + 0;
     }
     public static void main(String[] args) {
         System.out.println(Text.CLEAR_SCREEN);
         System.out.println(Text.HIDE_CURSOR);
         drawborder();
-        System.out.print(Text.RESET);
+        random()
     }
 }
