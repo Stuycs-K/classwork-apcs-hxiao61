62b96614b952c8438f19e4a853e12da8c31e1355
hxiao61 <hxiao61@stuy.edu>
2024-12-09 09:11:20 -0600

    did some debugging ill finish game.java later

---
+++ 2024-12-06-Adventurer/Game.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class Game{
+  CodeWarrior Jack = new CodeWarrior("Jack", 100);
+  Persona Joker = new Persona("Joker", 100, 120);
+  
+}
--- 2024-12-06-Adventurer/Persona.java
+++ 2024-12-06-Adventurer/Persona.java
@@ -4,10 +4,11 @@ public class Persona extends Adventurer{
     public Persona(String name, int hp, int sp){
         super(name, hp);
         this.SP = sp;
+        maxSP = sp;
     }
     @Override
     public String getSpecialName(){
-        return SP;
+        return "SP";
     }
     public int getSpecial(){
         return SP;
@@ -18,26 +19,26 @@ public class Persona extends Adventurer{
     public int getSpecialMax(){
         return maxSP;
     }
-    public abstract String attack(Adventurer other){
+    public String attack(Adventurer other){
         other.applyDamage(7);
         return getName() + " used a basic physical attack on " + other.getName() + " for 7 damage!";
     }
-    public abstract String support(Adventurer other){
+    public String support(Adventurer other){
         other.setHP(other.getHP() + 6);
         return getName() + " casts 'Dia' to heal " + other.getName() + " for 6 health!";
     }
-    public abstract String support(){
+    public String support(){
         setHP(getHP() + 6);
         return getName() + " casts 'Dia' to heal themselves for 6 health!";
     }
-    public abstract String specialAttack(Adventurer other){
+    public String specialAttack(Adventurer other){
         if(SP - 10 >= 0){
             SP = SP -3;
             other.applyDamage(80);
             return getName() + " casts 'Eigaon' on " + other.getName() + " for 80 curse damage!";
         }
         else {
-            return "Sorry " + getName() + "! Not enough mana!"; 
+            return "Sorry " + getName() + "! Not enough mana!";
         }
     }
 }
