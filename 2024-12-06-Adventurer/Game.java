ca81120c2e04c7a88c4999b16188989bfd46be9a
hxiao61 <hxiao61@stuy.edu>
2024-12-09 09:12:54 -0600

    forgot the main

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Game{
-  CodeWarrior Jack = new CodeWarrior("Jack", 100);
-  Persona Joker = new Persona("Joker", 100, 120);
-  
+  public static void main(String[] arg){
+    CodeWarrior Jack = new CodeWarrior("Jack", 100);
+    Persona Joker = new Persona("Joker", 100, 120);
+  }
 }
