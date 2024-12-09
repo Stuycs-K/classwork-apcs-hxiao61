7ca30653521ff215eda698f47b92d1cef31db4bd
hxiao61 <hxia61@stuy.edu>
2024-12-08 23:45:59 -0500

    finished ez

--- 2024-12-06-Adventurer/Persona.java
+++ 2024-12-06-Adventurer/Persona.java
@@ -19,15 +19,25 @@ public class Persona extends Adventurer{
         return maxSP;
     }
     public abstract String attack(Adventurer other){
-
+        other.applyDamage(7);
+        return getName() + " used a basic physical attack on " + other.getName() + " for 7 damage!";
     }
     public abstract String support(Adventurer other){
-
+        other.setHP(other.getHP() + 6);
+        return getName() + " casts 'Dia' to heal " + other.getName() + " for 6 health!";
     }
     public abstract String support(){
-
+        setHP(getHP() + 6);
+        return getName() + " casts 'Dia' to heal themselves for 6 health!";
     }
     public abstract String specialAttack(Adventurer other){
-        
+        if(SP - 10 >= 0){
+            SP = SP -3;
+            other.applyDamage(80);
+            return getName() + " casts 'Eigaon' on " + other.getName() + " for 80 curse damage!";
+        }
+        else {
+            return "Sorry " + getName() + "! Not enough mana!"; 
+        }
     }
 }
