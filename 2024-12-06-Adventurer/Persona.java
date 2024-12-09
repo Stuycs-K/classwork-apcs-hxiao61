658426b7dfe37e08645802f23fb43a944c1b2a28
Haowen Xiao <hxiao61@stuy.edu>
2024-12-08 23:32:57 -0500

    idk why it wont let me do it on git

--- 2024-12-06-Adventurer/Persona.java
+++ 2024-12-06-Adventurer/Persona.java
@@ -5,4 +5,29 @@ public class Persona extends Adventurer{
         super(name, hp);
         this.SP = sp;
     }
+    @Override
+    public String getSpecialName(){
+        return SP;
+    }
+    public int getSpecial(){
+        return SP;
+    }
+    public void setSpecial(int n){
+        SP = n;
+    }
+    public int getSpecialMax(){
+        return maxSP;
+    }
+    public abstract String attack(Adventurer other){
+
+    }
+    public abstract String support(Adventurer other){
+
+    }
+    public abstract String support(){
+
+    }
+    public abstract String specialAttack(Adventurer other){
+        
+    }
 }
