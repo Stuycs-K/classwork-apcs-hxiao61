83283d79687efe4284445984c6246d1241dbfe7d
hxiao61 <hxiao61@stuy.edu>
2025-01-03 09:15:09 -0600

    stuck on how to make the loop and stuff but will work on it at home

--- 2025-01-02-fibby/makeWords.java
+++ 2025-01-02-fibby/makeWords.java
@@ -3,11 +3,15 @@ public class makeWords{
 
   }
   public static void makeWords(int remainingLetters, String result, String alphabet){
+  int rem = alphabet.length();
+  while(rem > 0){
     if(remainingLetters > 0){
       System.out.println(makeWords(remainingLetters - 1, )
     }
     else {
       System.out.println(result);
+    }
+    rem--
     }
   }
 }
