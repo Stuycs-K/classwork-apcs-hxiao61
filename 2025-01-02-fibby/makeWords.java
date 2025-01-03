5dbf45f623a157dd7054ce816db8b478b68aa0d9
hxiao61 <hxiao61@stuy.edu>
2025-01-03 09:03:51 -0600

    adding the file

---
+++ 2025-01-02-fibby/makeWords.java
@@ -0,0 +1,13 @@
+public class makeWords{
+  public static void main(String[] args) {
+
+  }
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    if(remainingLetters > 0){
+      System.out.println(makeWords(remainingLetters - 1, )
+    }
+    else {
+      System.out.println(result);
+    }
+  }
+}
